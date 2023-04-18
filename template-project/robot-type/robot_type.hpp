d71ea5a5c06fd57e6fc4d4e2bc39488c237debfc
Aaron Wu <82164146+awu0@users.noreply.github.com>
2023-04-18 16:53:08 -0400

    don't put inline comments for robot_type.hpp

--- template-project/robot-type/robot_type.hpp
+++ template-project/robot-type/robot_type.hpp
@@ -1,5 +1,6 @@
 #ifndef __ROBOT_TYPE_HPP__
 #define __ROBOT_TYPE_HPP__
+// two motor - two shooter system
+#define TARGET_SENTRY
 // #define TARGET_STANDARD
-#define TARGET_SENTRY // two motor - two shooter system
 #endif
